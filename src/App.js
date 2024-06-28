e452ee5b100e40a143f4cafbb5cd37b13a404898
Jack Anderson <whotfisjax@gmail.com>
2024-06-28 02:19:32 -0400

    v0

--- src/App.js
+++ src/App.js
@@ -37,13 +37,13 @@ function App() {
           <div className='flex justify-center mt-[42.5%] translate-x-3 md:mt-[30%] md:translate-x-10'>
             <div className='grid content-center z-40'>
               <a
-                href="https://pump.fun/board"
+                href="https://pump.fun/99etatrYfgEmbXfLqWTFbMfBZGsWseMNx97Dwhanpump"
                 className='font-custom text-4xl md:text-5xl lg:text-6xl text-white bg-gradient-to-r from-[#5feb3e] via-[#80ff60] to-[#5feb3e] border-4 animate-text p-6 rounded-xl md:hover:scale-105 transition ease-in-out duration-200'
               >
                 BUY
               </a>
               <div className='pt-8 text-center font-custom text-2xl flex justify-center -mt-3'>
-                <a href="https://twitter.com/joeglorpden" className='p-2 md:hover:scale-110 transition ease-in-out duration-200'><img src={Xlogo} alt='Xlogo' className='w-12 h-12 rounded-md'></img></a>
+                <a href="https://x.com/joeglorpden" className='p-2 md:hover:scale-110 transition ease-in-out duration-200'><img src={Xlogo} alt='Xlogo' className='w-12 h-12 rounded-md'></img></a>
                 <a href="https://t.me/JoeGlorpden" className='p-2 md:hover:scale-110 transition ease-in-out duration-200'><img src={TG} alt='Tg logo' className='w-12 h-12'></img></a>
               </div>
             </div>
